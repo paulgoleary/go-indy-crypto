86818964b381f618e084170a321811de62f77dd3
Paul O'Leary <paul@eluv.io>
2020-03-14 18:24:56 -0700

    More issuer wrapping work and tests.

--- issuer/credential.go
+++ issuer/credential.go
@@ -1,8 +1,8 @@
 package issuer
 
 /*
-#cgo LDFLAGS: -L${SRCDIR}/../../../libindy-crypto/target/debug -lindy_crypto
-#cgo CFLAGS: -I${SRCDIR}/../../../libindy-crypto/include
+#cgo LDFLAGS: -L${SRCDIR}/../../../hyperledger/indy-crypto/libindy-crypto/target/debug -lindy_crypto
+#cgo CFLAGS: -I${SRCDIR}/../../../hyperledger/indy-crypto/libindy-crypto/include
 
 #include <stdlib.h>
 #include "indy_crypto.h"
@@ -11,6 +11,19 @@ extern int indy_crypto_cl_credential_schema_builder_new(void**);
 extern int indy_crypto_cl_credential_schema_builder_add_attr(void*, const char*);
 extern int indy_crypto_cl_credential_schema_builder_finalize(void *, void**);
 extern int indy_crypto_cl_credential_schema_free(void*);
+
+extern int indy_crypto_cl_non_credential_schema_builder_new(void**);
+extern int indy_crypto_cl_non_credential_schema_builder_add_attr(void*, const char*);
+extern int indy_crypto_cl_non_credential_schema_builder_finalize(void *, void**);
+extern int indy_crypto_cl_non_credential_schema_free(void*);
+
+extern int indy_crypto_cl_issuer_new_credential_def(void*, void*, int, void**, void**, void**);
+extern int indy_crypto_cl_credential_public_key_to_json(void*, const char**);
+extern int indy_crypto_cl_credential_public_key_free(void*);
+extern int indy_crypto_cl_credential_private_key_to_json(void*, const char**);
+extern int indy_crypto_cl_credential_private_key_free(void*);
+extern int indy_crypto_cl_credential_key_correctness_proof_to_json(void*, const char**);
+extern int indy_crypto_cl_credential_key_correctness_proof_free(void*);
 
 */
 import "C"
@@ -23,8 +36,22 @@ type CredSchemaBuilder struct {
 	sb unsafe.Pointer
 }
 
+type NonCredSchemaBuilder struct {
+	sb unsafe.Pointer
+}
+
 type CredSchema struct {
 	cs unsafe.Pointer
+}
+
+type NonCredSchema struct {
+	cs unsafe.Pointer
+}
+
+type CredentialDef struct {
+	pk unsafe.Pointer
+	sk unsafe.Pointer
+	cp unsafe.Pointer
 }
 
 // TODO: map returns codes to 'common errors' from errors/mod.rs
@@ -35,19 +62,14 @@ func withErr(ret C.int) error {
 	return nil
 }
 
-func MakeSchemaBuilder() (*CredSchemaBuilder, error) {
+// CredSchemaBuilder
+
+func MakeCredSchemaBuilder() (*CredSchemaBuilder, error) {
 	ret := CredSchemaBuilder{}
 	if err := withErr(C.indy_crypto_cl_credential_schema_builder_new(&ret.sb)); err != nil {
 		return nil, err
 	}
 	return &ret, nil
-}
-
-func (sb *CredSchemaBuilder) Close() {
-	if sb.sb != nil {
-		C.indy_crypto_cl_credential_schema_free(sb.sb)
-		sb.sb = nil
-	}
 }
 
 func (sb *CredSchemaBuilder) AddAttrib(attribName string) error {
@@ -61,5 +83,106 @@ func (sb *CredSchemaBuilder) Finalize() (*CredSchema, error) {
 	if err := withErr(C.indy_crypto_cl_credential_schema_builder_finalize(sb.sb, &ret.cs)); err != nil {
 		return nil, err
 	}
+	sb.sb = nil // finalize free's the builder
 	return &ret, nil
+}
+
+// CredSchema
+
+func (cs *CredSchema) Close() {
+	if cs.cs != nil {
+		C.indy_crypto_cl_credential_schema_free(cs.cs)
+		cs.cs = nil
+	}
+}
+
+// NonCredSchemaBuilder
+
+func MakeNonCredSchemaBuilder() (*NonCredSchemaBuilder, error) {
+	ret := NonCredSchemaBuilder{}
+	if err := withErr(C.indy_crypto_cl_non_credential_schema_builder_new(&ret.sb)); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+func (sb *NonCredSchemaBuilder) AddAttrib(attribName string) error {
+	nameCBytes := C.CBytes([]byte(attribName))
+	defer C.free(nameCBytes)
+	return withErr(C.indy_crypto_cl_non_credential_schema_builder_add_attr(sb.sb, (*C.char)(nameCBytes)))
+}
+
+func (sb *NonCredSchemaBuilder) Finalize() (*NonCredSchema, error) {
+	ret := NonCredSchema{}
+	if err := withErr(C.indy_crypto_cl_non_credential_schema_builder_finalize(sb.sb, &ret.cs)); err != nil {
+		return nil, err
+	}
+	sb.sb = nil // finalize free's the builder
+	return &ret, nil
+}
+
+// NonCredSchema
+
+func (cs *NonCredSchema) Close() {
+	if cs.cs != nil {
+		C.indy_crypto_cl_non_credential_schema_free(cs.cs)
+		cs.cs = nil
+	}
+}
+
+// CredentialDef
+
+func MakeCredentialDef(credSchema *CredSchema, nonCredSchema *NonCredSchema, withRevoke bool) (*CredentialDef, error) {
+	ret := CredentialDef{}
+	var wr C.int = 0
+	if withRevoke {
+		wr = 1
+	}
+	if err := withErr(C.indy_crypto_cl_issuer_new_credential_def(credSchema.cs, nonCredSchema.cs, wr, &ret.pk, &ret.sk, &ret.cp)); err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+func (cd *CredentialDef) Close() {
+	if cd.pk != nil {
+		C.indy_crypto_cl_credential_public_key_free(cd.pk)
+		cd.pk = nil
+	}
+	if cd.sk != nil {
+		C.indy_crypto_cl_credential_private_key_free(cd.sk)
+		cd.sk = nil
+	}
+	if cd.cp != nil {
+		C.indy_crypto_cl_credential_key_correctness_proof_free(cd.cp)
+		cd.cp = nil
+	}
+}
+
+func (cd *CredentialDef) GetPublicKeyJson() (string, error) {
+	var jsonCStr *C.char
+	if err := withErr(C.indy_crypto_cl_credential_public_key_to_json(cd.pk, &jsonCStr)); err != nil {
+		return "", err
+	}
+	defer C.free(unsafe.Pointer(jsonCStr))
+	return C.GoString(jsonCStr), nil
+}
+
+func (cd *CredentialDef) GetSecretKeyJson() (string, error) {
+	var jsonCStr *C.char
+	if err := withErr(C.indy_crypto_cl_credential_private_key_to_json(cd.sk, &jsonCStr)); err != nil {
+		return "", err
+	}
+	defer C.free(unsafe.Pointer(jsonCStr))
+	return C.GoString(jsonCStr), nil
+
+}
+
+func (cd *CredentialDef) GetProofJson() (string, error) {
+	var jsonCStr *C.char
+	if err := withErr(C.indy_crypto_cl_credential_key_correctness_proof_to_json(cd.cp, &jsonCStr)); err != nil {
+		return "", err
+	}
+	defer C.free(unsafe.Pointer(jsonCStr))
+	return C.GoString(jsonCStr), nil
 }
--- issuer/credential_test.go
+++ issuer/credential_test.go
@@ -7,10 +7,9 @@ import (
 
 func TestBasic(t *testing.T) {
 
-	credBuilder, err := MakeSchemaBuilder()
+	credBuilder, err := MakeCredSchemaBuilder()
 	require.NoError(t, err)
 	require.NotNil(t, credBuilder)
-	defer credBuilder.Close()
 
 	err = credBuilder.AddAttrib("name")
 	require.NoError(t, err)
@@ -24,5 +23,35 @@ func TestBasic(t *testing.T) {
 	credSchema, err := credBuilder.Finalize()
 	require.NoError(t, err)
 	require.NotNil(t, credSchema)
+	defer credSchema.Close()
+
+	nonCredBuilder, err := MakeNonCredSchemaBuilder()
+	require.NoError(t, err)
+	require.NotNil(t, nonCredBuilder)
+
+	err = nonCredBuilder.AddAttrib("master_secret")
+	require.NoError(t, err)
+
+	nonCredSchema, err := nonCredBuilder.Finalize()
+	require.NoError(t, err)
+	require.NotNil(t, nonCredSchema)
+	defer nonCredSchema.Close()
+
+	credDef, err := MakeCredentialDef(credSchema, nonCredSchema, true)
+	require.NoError(t, err)
+	require.NotNil(t, credDef)
+	defer credDef.Close()
+
+	pkJsonStr, err := credDef.GetPublicKeyJson()
+	require.NoError(t, err)
+	println(pkJsonStr)
+
+	skJsonStr, err := credDef.GetSecretKeyJson()
+	require.NoError(t, err)
+	println(skJsonStr)
+
+	proofJsonStr, err := credDef.GetProofJson()
+	require.NoError(t, err)
+	println(proofJsonStr)
 
 }
